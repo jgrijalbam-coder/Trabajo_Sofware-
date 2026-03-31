ad4c5c0b6579985ce779a70d06375fe57bad4115
jgrijalbam-coder <jgrijalbam@ucentral.edu.co>
2026-03-31 17:24:25 -0500

    Add routes for user retrieval and update

--- Documents/Trabajo/Trabajo_Software_II/backend/routes/usuarios.routes.js
+++ Documents/Trabajo/Trabajo_Software_II/backend/routes/usuarios.routes.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {
   listarUsuarios,
+  obtenerUsuarioPorId,
   registrarUsuario,
-  loginUsuario
+  loginUsuario,
+  actualizarUsuario
 } = require('../controllers/usuarios.controller');
 
 router.get('/', listarUsuarios);
+router.get('/:id', obtenerUsuarioPorId);
 router.post('/registro', registrarUsuario);
 router.post('/login', loginUsuario);
+router.put('/:id', actualizarUsuario);
 
 module.exports = router;
